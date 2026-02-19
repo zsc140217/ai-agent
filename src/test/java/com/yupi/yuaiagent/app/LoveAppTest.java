f0d9ffcfd78978c29a97485e48c2c72d2a21a80e
zsc140217 <1402175551@qq.com>
2026-02-19 12:07:58 +0800

    自定义mcp应用

--- src/test/java/com/yupi/yuaiagent/app/LoveAppTest.java
+++ src/test/java/com/yupi/yuaiagent/app/LoveAppTest.java
@@ -76,11 +76,7 @@ class LoveAppTest {
     void doChatWithMcp() {
         String chatId = UUID.randomUUID().toString();
         // 测试地图 MCP
-//        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
-//        String answer =  loveApp.doChatWithMcp(message, chatId);
-//        Assertions.assertNotNull(answer);
-        // 测试图片搜索 MCP
-        String message = "帮我搜索一些哄另一半开心的图片";
+        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
         String answer =  loveApp.doChatWithMcp(message, chatId);
         Assertions.assertNotNull(answer);
     }
